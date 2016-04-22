6d19e159444d2c6cd07de737ab5e3d6e20582557
Pavel910 <pavel@webiny.com>
2016-04-22 12:36:43 +0200

    Moved eslint rules to separate file

--- install/structure/gulp/config.js
+++ install/structure/gulp/config.js
@@ -42,46 +42,6 @@ module.exports = function (gulp, opts, $, pipes) {
             images: function (sourceAppDir) {
                 return parseValues(paths.images, sourceAppDir);
             }
-        },
-        eslint: {
-            "extends": "airbnb",
-            "rules": {
-                "react/no-multi-comp": 0,
-                "react/react-in-jsx-scope": 0,
-                "react/sort-comp": 0,
-                "react/prop-types": 0,
-                "react/jsx-boolean-value": 0,
-                "indent": 0,
-                "comma-dangle": [2, "never"],
-                "object-curly-spacing": 0,
-                "new-cap": 0,
-                "no-param-reassign": 0,
-                "radix": [2, "as-needed"],
-                "no-console": 0,
-                "eol-last": 0,
-                "dot-notation": 0,
-                "no-script-url": 0
-            },
-            "globals": {
-                "jQuery": false,
-                "$": false,
-                "Cookies": false,
-                "Webiny": true,
-                "WebinyBootstrap": true,
-                "React": false,
-                "ReactDOM": false,
-                "ReactDOMServer": false,
-                "Bottle": false,
-                "Baobab": false,
-                "_": false,
-                "Q": false,
-                "axios": false,
-                "moment": false,
-                "accounting": false,
-                "_apiUrl": true,
-                "isMobile": false,
-                "filesize": true
-            }
         }
     };
 };
